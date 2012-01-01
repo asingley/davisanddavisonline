15baea7dd494fe18a5a743eb311178d707f16eb6
asingley <asingley@linux.com>
2012-01-01 00:19:12 -0500

    center that thang

--- intro.php
+++ intro.php
@@ -10,12 +10,16 @@ setcookie("no_splash", "1", $expire); // This makes the cookie. It goes in this 
 <link rel="stylesheet" href="intro-css.css" type="text/css">
 
 <div id="intro-anim">
-<iframe src="intro-animation.php" width="1000" height="240"
+<iframe src="intro-animation.php" width="1000" height="300"
              scrolling="no" frameborder="0">
 TEXT FOR NON-COMPATIBLE BROWSERS HERE</iframe>
 
 <div id="enter">
+
+<center>
 <a href="index.php" >Enter Site</a> 
+</center>
+
 </div>
 </div>
 </html>
