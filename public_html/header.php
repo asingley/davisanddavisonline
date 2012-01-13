0b8094f6865d468b8d3d0b03cda7d99e9c976c63
Andy Singley <asingley@m17x.(none)>
2012-01-12 23:52:01 -0500

    twitterlogin

--- public_html/header.php
+++ public_html/header.php
@@ -10,17 +10,57 @@
 	<script type="text/javascript" src="flash/AC_RunActiveContent.js" language="javascript"></script>
 	<script type="text/javascript" language="javascript" src="scripter.js"></script>
 	
-	<script src="../jquery.js"></script>
-<script  src="../js/jquery.anchorCloud.js"> </script>
 </head>
 <body>
 
+<div id="container">
+  <div id="topnav" class="topnav"> Have an account? <a href="login" class="signin"><span>Sign in</span></a> </div>
+  <fieldset id="signin_menu">
+    <form method="post" id="signin" action="https://twitter.com/sessions">
+      <label for="username">Username or email</label>
+      <input id="username" name="username" value="" title="username" tabindex="4" type="text">
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" value="" title="password" tabindex="5" type="password">
+      </p>
+      <p class="remember">
+        <input id="signin_submit" value="Sign in" tabindex="6" type="submit">
+        <input id="remember" name="remember_me" value="1" tabindex="7" type="checkbox">
+        <label for="remember">Remember me</label>
+      </p>
+      <p class="forgot"> <a href="#" id="resend_password_link">Forgot your password?</a> </p>
+      <p class="forgot-username"> <A id=forgot_username_link 
+title="If you remember your password, try logging in with your email" 
+href="#">Forgot your username?</A> </p>
+    </form>
+  </fieldset>
+</div>
+
+
+<script src="../javascripts/jquery.js" type="text/javascript"></script>
 <script type="text/javascript">
-$(document).ready(function()
-{
-$(body).anchorCloud();
-});
+        $(document).ready(function() {
+
+            $(".signin").click(function(e) {
+                e.preventDefault();
+                $("fieldset#signin_menu").toggle();
+                $(".signin").toggleClass("menu-open");
+            });
+
+            $("fieldset#signin_menu").mouseup(function() {
+                return false
+            });
+            $(document).mouseup(function(e) {
+                if($(e.target).parent("a.signin").length==0) {
+                    $(".signin").removeClass("menu-open");
+                    $("fieldset#signin_menu").hide();
+                }
+            });            
+
+        });
 </script>
+
 
 <noscript>You must enable scripting in your browser settings to navigate and order on this site. 
 </noscript>
