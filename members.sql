50df099c5dd200d05f846672fc4ba4c9c8a6c02b
Andy Singley <asingley@m17x.(none)>
2012-01-12 22:55:13 -0500

    members

---
+++ members.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `members` (
+`id` int(4) NOT NULL auto_increment,
+`username` varchar(65) NOT NULL default '',
+`password` varchar(65) NOT NULL default '',
+PRIMARY KEY (`id`)
+) TYPE=MyISAM AUTO_INCREMENT=2 ;
