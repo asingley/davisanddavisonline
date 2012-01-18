d3568103864f92cbc4124bc85441799dcb1bede4
Andy Singley <asingley@m17x.(none)>
2012-01-17 19:52:15 -0500

    admin stuff

--- public_html/submit_new_item.php
+++ public_html/submit_new_item.php
@@ -1,2 +1,22 @@
 <?php
+$con = mysql_connect("localhost","davis","davis");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("davisanddavis", $con);
+
+$sql="INSERT INTO products (product_name, description, recipe, cost, active)
+VALUES
+('$_POST[prod_name]','$_POST[prod_desc]','$_POST[prod_recip]','$_POST[prod_price]')";
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+
+mysql_close($con);
+header('Location: new_item_form.php');
 ?>
