6f2fa7c275ae2bd3e33c71af0a361816661513dd
Andy Singley <asingley@m17x.(none)>
2012-01-19 12:37:12 -0500

    moved panel

--- public_html/header.php
+++ public_html/header.php
@@ -23,49 +23,7 @@
 
 
 
-<div style="text-align:right;font-weight:bold;">
-<div id="container">
-  <div id="topnav" class="topnav"><a href="login" class="signin"><span>Admin</span></a>   <a href="dd_catalog.pdf" target="newWin">View the 2010 Catalog</a></div>
-  <fieldset id="signin_menu">
-    <form method="post" id="signin" action="http://slomaro34.servebeer.com/public_html/check_login.php">
-      <label for="username">Username</label>
-      <input id="username" name="myusername" value="" title="username" tabindex="4" type="text">
-      </p>
-      <p>
-        <label for="password">Password</label>
-        <input id="password" name="mypassword" value="" title="password" tabindex="5" type="password">
-      </p>
-      <p class="remember">
-        <input id="submit" value="Login" name="Submit" tabindex="6" type="submit">
-      </p>
-    </form>
-  </fieldset>
-
-</div>
-
-
-<script src="../javascripts/jquery.js" type="text/javascript"></script>
-<script type="text/javascript">
-        $(document).ready(function() {
-
-            $(".signin").click(function(e) {
-                e.preventDefault();
-                $("fieldset#signin_menu").toggle();
-                $(".signin").toggleClass("menu-open");
-            });
-
-            $("fieldset#signin_menu").mouseup(function() {
-                return false
-            });
-            $(document).mouseup(function(e) {
-                if($(e.target).parent("a.signin").length==0) {
-                    $(".signin").removeClass("menu-open");
-                    $("fieldset#signin_menu").hide();
-                }
-            });            
-
-        });
-</script>
+<div id="topnav" class="topnav"><a href="dd_catalog.pdf" target="newWin">View the 2010 Catalog</a></div>
 
 </div>
     
