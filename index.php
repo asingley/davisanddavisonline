9636039d0faf2eb872fcb51f5cf4a15adb36a372
Andy Singley <asingley@m17x.(none)>
2012-01-20 23:21:19 -0500

    removed admin

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+echo '<link type="text/css" rel="Stylesheet" href="../styler.css" />';
+echo '<div id="centerBlock">';
 if(!isset($_COOKIE['no_splash'])) {
 	// Start of the IF statement - if the user does not have a cookie that says whether he/she has visited the splash page, take them to the splash page
 	header('Location: intro.php'); // Redirect code
@@ -54,4 +56,5 @@ else {
 }
 require("public_html/footer.php");
 }
+echo '</div>';
 ?>
