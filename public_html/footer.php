e75f62b7e8e15c14135167291a54003a8b6e1d79
Andy Singley <asingley@m17x.(none)>
2012-01-20 23:52:33 -0500

    removed admin

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,81 +1,30 @@
-<style type="text/css">
-<!--
-.style1 {font-family: Verdana, Arial, Helvetica, sans-serif}
--->
-</style>
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td width="48%"><div align="center">
-      <p align="left"><span class="footer"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer"><a href="contact.php"><font color="#999999" size="1">Contact</font></a>&nbsp; | &nbsp;<a href="faqs.php"><font color="#999999" size="1">Customer
-              Relations</font></a> &nbsp;|&nbsp; <a href="mailto:feedback02@davisanddavisonline.com"><font color="#999999" size="1">Feedback</font></a>&nbsp;|&nbsp; <font color="#999999" size="1"><a href="policies.php"><font color="#999999" size="1">Policies</font></a></font><br />
-            <br />
-		  <font color="#666666"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer"><img src="../img/spacer.gif" alt="sapcer">&copy; 
-		  2012 Davis &amp; Davis Gourmet Foods </font><br />
-            
-
-<div id="container">
-  <a href="login" class="signin"><span>Admin</span></a> 
-  <fieldset id="signin_menu">
-    <form method="post" id="signin" action="http://slomaro34.servebeer.com/public_html/check_login.php">
-      <label for="username">Username</label>
-      <input id="username" name="myusername" value="" title="username" tabindex="4" type="text">
-      </p>
-      <p>
-        <label for="password">Password</label>
-        <input id="password" name="mypassword" value="" title="password" tabindex="5" type="password">
-      </p>
-      <p class="remember">
-        <input id="submit" value="Login" name="Submit" tabindex="6" type="submit">
-      </p>
-    </form>
-  </fieldset>
-
-
-
-
-<script src="../javascripts/jquery.js" type="text/javascript"></script>
-<script type="text/javascript">
-        $(document).ready(function() {
-
-            $(".signin").click(function(e) {
-                e.preventDefault();
-                $("fieldset#signin_menu").toggle();
-                $(".signin").toggleClass("menu-open");
-            });
-
-            $("fieldset#signin_menu").mouseup(function() {
-                return false;
-            });
-            $(document).mouseup(function(e) {
-                if($(e.target).parent("a.signin").length==0) {
-                    $(".signin").removeClass("menu-open");
-                    $("fieldset#signin_menu").hide();
-                }
-            });            
-
-        });
-</script>
-            
-      <p>&nbsp;</p>
-    </div></td>
-    <td width="50%" valign="top"><!-- BEGIN: Constant Contact HTML for OptIn Tag  -->
-<center>
-<form name="ccoptin" action="http://ui.constantcontact.com/roving/d.jsp" target="_blank" method="post">
-<table bgcolor="lightyellow" border="1" bordercolor="#ABC36E" cellpadding="3" cellspacing="0">
-<tr><td align="center"><span class="style1"><font color="#999999" size="1">Join
-        the�<b>Davis & Davis Gourmet Foods</b>�mailing list�</font></span></td>
-</tr>
-<tr><td align="center"><span class="style1"><font color="#999999" size="1"><b>Email:</b>
-        <input type=text name="ea" size=25>
-        <input type=hidden name="m" value="1100374215763">
-        <input type=hidden name="p" value="oi">
-        <input type=submit name="go" value="Go">
-</font></span></td>
-</tr>
-</table></form>
-</center>
-<!-- End: Constant Contact HTML for OptIn Tag --></td>
-  </tr>
-</table>
-
-
+<div class="purpBar">
+					<img src="images/purplefadebar.jpg" alt="Davis And Davis" />
+				</div>
+				<div id="footer">
+					<span class="smallText"><a href="about.htm">About</a></span>
+					<span class="smallText"><a href="contact.htm">Contact</a></span>
+					<span class="smallText"><a href="relations.htm">Customer Relations</a></span>
+					<span class="smallText"><a href="mailto:feedback02@davisanddavisonline.com">Feedback</a></span>
+					<span class="smallText"><a href="policies.htm">Policies</a></span>
+				</div>
+				<form name="ccoptin" action="http://ui.constantcontact.com/roving/d.jsp" target="_blank" method="post">
+					<div style="text-align:center;">
+						<div>
+							Join our mailing list: <input onclick="fClearMailer(this);" style="background-color:#e6d0f0;" type="text" name="ea" size="25" value="Enter Your Email" />
+							<input type="hidden" name="m" value="1100374215763" />
+							<input type="hidden" name="p" value="oi" />
+							<input type="submit" name="go" value="Go" />
+						</div>
+					</div>
+				</form>
+				<div id="note">
+					&copy;2012 Davis &amp; Davis Gourmet Foods<br />
+				</div>
+				<form id="fm" action="http://www.davisanddavisonline.com/cgi-davisanddavisonline/sb/order.cgi" method="post">
+				<input id="storeid" name="storeid" type="hidden" value="" />
+				<input id="super" name="super" type="hidden" value="" />
+				<input id="dbname" name="dbname" type="hidden" value="" />
+				<input id="function" name="function" type="hidden" value="" />
+				<input id="itemnum" name="itemnum" type="hidden" value="" />
+				</form>
