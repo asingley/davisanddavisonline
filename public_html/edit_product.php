1f2f3ff84d9dbe55382315a827b4fc32792ec5ef
Andy Singley <asingley@m17x.(none)>
2012-01-22 02:38:02 -0500

    edit page added

--- public_html/edit_product.php
+++ public_html/edit_product.php
@@ -19,18 +19,22 @@ $row = mysql_fetch_array($result);
 
 <form enctype="multipart/form-data" action="submit_new_item.php" method="post"></br>
 Product Name: <input type="text" name="prod_name" value="<?php echo $row['product_name'];?>"/></br>
+<?php 
+if($row['prod_type'] == "alone")
+	echo '<input type="radio" name="group1" value="alone" checked> Stand Alone Item<br>';
+else 
+	echo '<input type="radio" name="group1" value="alone" checked> Stand Alone Item<br>';
 
-<input type="radio" name="group1" value="alone"> Stand Alone Item<br>
-<input type="radio" name="group1" value="drink"> Drinks<br>
-<input type="radio" name="group1" value="bowl"> Bowls<br>
-<input type="radio" name="group1" value="cball"> Cheeseball Kits<br>
-<input type="radio" name="group1" value="oil"> Cooking and Dipping Oils<br>
-<input type="radio" name="group1" value="dip"> Dipper Mixes<br>
-<input type="radio" name="group1" value="fdrink"> Frozen Drinks and Mixes<br>
-<input type="radio" name="group1" value="kstuff"> Ken's Stuff<br>
-<input type="radio" name="group1" value="must" > Mustards and Relishes<br>
-<input type="radio" name="group1" value="salsa"> Salsa Magic<br>
-
+echo '<input type="radio" name="group1" value="drink"> Drinks<br>';
+echo '<input type="radio" name="group1" value="bowl"> Bowls<br>';
+echo '<input type="radio" name="group1" value="cball"> Cheeseball Kits<br>';
+echo '<input type="radio" name="group1" value="oil"> Cooking and Dipping Oils<br>';
+echo '<input type="radio" name="group1" value="dip"> Dipper Mixes<br>';
+echo '<input type="radio" name="group1" value="fdrink"> Frozen Drinks and Mixes<br>';
+echo '<input type="radio" name="group1" value="kstuff"> Ken\'s Stuff<br>';
+echo '<input type="radio" name="group1" value="must" > Mustards and Relishes<br>';
+echo '<input type="radio" name="group1" value="salsa"> Salsa Magic<br>';
+?>
 Product Description: <textarea cols="50" rows="4" name="prod_desc" value="<?php echo $row['description'];?>"></textarea><br>
 Product Recipe: <input type="text" name="prod_recip" value="<?php echo $row['recipe'];?>"/><br>
 Price Per Unit: <input type="text" name="prod_price" value="<?php echo $row['cost'];?>"/><br>
