c06de7a7b0a2142ee7b21d85aa9699d74c6fd21b
Andy Singley <asingley@m17x.(none)>
2012-01-21 23:42:33 -0500

    added upload

--- public_html/submit_new_item.php
+++ public_html/submit_new_item.php
@@ -8,6 +8,7 @@ $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 } else{
 	echo "There was an error uploading the file, please try again!";
+	echo $_FILES['uploadedfile']['tmp_name'] . "target:" . $target_path;
 }
 
 
