e4f99fb328bd24ee08e19b80196414a7913024e4
Andy Singley <asingley@m17x.(none)>
2012-01-21 21:05:25 -0500

    admin changes

--- public_html/new_item_form.php
+++ public_html/new_item_form.php
@@ -1,7 +1,19 @@
 
 <form action="submit_new_item.php" method="post"></br>
 Product Name: <input type="text" name="prod_name"/></br>
-Product Description: <input type="text" name="prod_desc"/></br>
+<div align="center"><br>
+<input type="radio" name="group1" value="alone" checked> Stand Alone Item<br>
+<input type="radio" name="group1" value="drink"> Drinks<br>
+<input type="radio" name="group1" value="bowl"> Bowls<br>
+<input type="radio" name="group1" value="cball"> Cheeseball Kits<br>
+<input type="radio" name="group1" value="oil"> Cooking and Dipping Oils<br>
+<input type="radio" name="group1" value="dip"> Dipper Mixes<br>
+<input type="radio" name="group1" value="fdrink"> Frozen Drinks and Mixes<br>
+<input type="radio" name="group1" value="kstuff"> Ken's Stuff<br>
+<input type="radio" name="group1" value="must" > Mustards and Relishes<br>
+<input type="radio" name="group1" value="salsa"> Salsa Magic
+<hr>
+Product Description: <textarea cols="50" rows="4" name="prod_desc"></textarea></br>
 Product Recipe: <input type="text" name="prod_recip"/></br>
 Price Per Unit: <input type="text" name="prod_price"/></br>
 <input type="submit" value="Submit"/>
