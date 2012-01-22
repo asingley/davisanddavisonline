b61fb4d934003472cdc53270877cb4e5307268e2
Andy Singley <asingley@m17x.(none)>
2012-01-22 00:55:49 -0500

    added folder

--- admin.php
+++ admin.php
@@ -37,7 +37,7 @@ while($row = mysql_fetch_row($result)) {
 	for($column_num = 0; $column_num < $column_count; $column_num++) {
 		print("<TD>$row[$column_num]&nbsp;</TD>\n");
 	}
-	echo "<TD><a href="'edit_product.php'">edit item</a></TD>\n";
+	//echo "<TD><a href="'edit_product.php'">edit item</a></TD>\n";
 	print("</TR>\n");
 }
 print("</TABLE>\n");
