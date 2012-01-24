8c7cf9f8fe819f55579534cc17646fd2633aa16c
Andy Singley <asingley@m17x.(none)>
2012-01-24 01:06:39 -0500

    edit

--- product_generator.php
+++ product_generator.php
@@ -1,3 +1,12 @@
+<script type="text/javascript" src="scripter.js"></script>
+<link rel="stylesheet" type="text/css" href="product.css" />
 <?php 
+require_once("public_html/db_connect.php");
+$sql= "SELECT * FROM products where id=$proid";
+$result = mysql_query($sql);
+$row = mysql_fetch_array($result);
+
+
 echo $proid;
+require_once("public_html/db_close.php");
 ?>
