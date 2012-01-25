b27f4796706842264512ca63d577978310df6850
Andy Singley <asingley@m17x.(none)>
2012-01-24 23:32:12 -0500

    edit

--- product_generator.php
+++ product_generator.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type="text/css" href="product.css" />
 <?php 
 require_once("public_html/db_connect.php");
-$sql= "SELECT * FROM products where id=1";
+$sql= "SELECT * FROM products where id='$proid'";
 $result = mysql_query($sql);
 $row = mysql_fetch_array($result);
 
