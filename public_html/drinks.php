0517cf876f0de03975c7f5e0d695987be0d92790
Andy Singley <asingley@m17x.(none)>
2012-01-26 01:37:16 -0500

    admin changes

--- public_html/drinks.php
+++ public_html/drinks.php
@@ -1,3 +1,57 @@
-<?php 
-echo 'hello';
-?>
+
+<div id="panel">
+<div id="prodTitle">
+<div>Frozen Drink Mixes</div>
+</div>
+
+<!--BAR-->
+<div class="purpBar">
+<img src="images/purplefadebar.jpg" alt="" />
+</div>
+<div id="prodDisp">
+<div><img src="images/frozenGroup.jpg" alt="Frozen Drink Mix Group Shot" /></div>
+</div>
+<div id="prodInfoText">
+			<div>
+WOW! ...over 1/2 gallon of perfectly blended cocktails each and every time you make a Davis &amp; Davis Gourmet Foods Frozen Drink Mix. Pick your favorite. There’s a flavor for just about everyone’s taste buds.  Cosmopolitan, Lemon Drop, Margarita, Strawberry Margarita, Tango Tini, Chocolate Tini, Peach Bellini, Mimosa, Sangria or Wine Magic.
+</div>
+<div>
+<div style="font-weight:bold;text-decoration:underline;margin-top:12px;"><br />Frozen Drink Mix Directions</div>
+<div><br />
+Combine contents of Magic Drink Mix bag with 6 cups of water and 2 cups of your favorite white wine, vodka, rum or tequila in a one gallon freezer safe zip bag. Blend well and place in the freezer. Freeze for 3-4 hours, remove from freezer and squeeze the bag, enjoy!
+</div>
+</div>
+</div>
+<p style="text-align:left;margin-left:20px;"><b>SAVE!!!</b> Buy a Multi-Pack and choose any 2 or 6 flavor combinations of Frozen Drink Mixes!
+Just choose the 2 or 6 Pack option in the drop-down, click the cart, and then select your drink mixes in the shopping cart...</p>
+<p>
+<div style="text-align:center;font-weight:bold;text-decoration:underline;">ORDERING OPTIONS</div>
+			<div style="text-align:center;margin-top:14px;">
+				<table style="margin:auto;width:200px;">
+<tr>
+						<td>
+							<select id="selOpt">
+<option value="13">Chocolate-Tini: $11.00</option>
+<option value="69">Cosmopolitan: $11.00</option>
+<option value="76">Lemon Drop: $11.00</option>
+<option value="82">Margarita: $11.00</option>
+<option value="143">Mimosa: $11.00</option>
+<option value="142">Peach Bellini: $11.00</option>
+<option value="144">Sangria: $11.00</option>
+<option value="10">Strawberry Margarita: $11.00</option>
+<option value="28">Tango-Tini: $11.00</option>
+<option value="112">Wine Magic: $11.00</option>
+<optgroup label="Create a Multi-Pack">
+<option value="118">2 Pack: $20.00</option>
+<option value="27">6 Pack: $50.00</option>
+								</optgroup>
+</select>
+</td>
+<td>
+							<img onclick="fAddItem('drink')" class="cart" src="images/cart.gif" alt="Add this selection to your basket." />
+</td>
+</tr>
+</table>
+</div>
+</p>
+	</div>
