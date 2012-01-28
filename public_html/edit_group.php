3721c5a82a5f4d6994b8e14bb6dbcb24c48b617e
Andy Singley <asingley@m17x.(none)>
2012-01-28 00:50:19 -0500

    tab js

---
+++ public_html/edit_group.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if(!session_is_registered(myusername)){
+	header("location:index.php");
+}?>
+<head>
+<title>New Group Portal</title>
+</head>
+
+<H1>New Group Portal</H1>
+<hr>
+
+<form enctype="multipart/form-data" action="submit_edit_group.php" method="post"></br>
+Group Name: <input type="text" name="group_name"/></br>
+Group Short Name: <input type="text" name="short_name"/></br>
+
+
+
+
+<script type="text/javascript">
+function formatText (el,tag) {
+var selectedText=document.selection?document.selection.createRange().text:el.value.substring(el.selectionStart,el.selectionEnd);
+if (selectedText!='') {
+var newText='<'+tag+'>'+selectedText+'</'+tag+'>';
+el.value=el.value.replace(selectedText,newText)
+}
+} 
+</script>
+
+Group Description:<br>
+<textarea name="group_desc" rows = "12" cols = "50"></textarea><br>
+<input type="button" value="Bold" onclick="formatText (group_desc,'b');" />
+<input type="button" value="Italic" onclick="formatText (group_desc,'i');" />
+<input type="button" value="Underline" onclick="formatText (group_desc,'u');" />
+
+
+<br>
+
+<br>
+<input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
+Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+
+<br>
+<h4>Active:</h4>
+<?php 
+if ($row['active'] == 1){
+	echo '<input type="radio" name="group2" value="1" checked> Active<br>';
+	echo '<input type="radio" name="group2" value="0"> Not Active(No Longer a Valid Group)<br>';}
+else{
+	echo '<input type="radio" name="group2" value="1" > Active<br>';
+	echo '<input type="radio" name="group2" value="0" checked> Not Active(No Longer a Valid Group)<br>';
+}
+?>
+
+<input type="submit" value="Submit"/><a href="../admin.php">Return to Admin Home</a>
+</form>
---
+++ public_html/submit_edit_group.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if(!session_is_registered(myusername)){
+	header("location:index.php");
+}
+$target_path = "../img/";
+
+/* Add the original filename to our target path.
+ Result is "uploads/filename.extension" */
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+$description = base64_encode(nl2br(str_replace('  ', ' &nbsp;', htmlspecialchars($_POST[group_desc]))));
+$name = base64_encode($_POST[group_name]);
+if (basename( $_FILES['uploadedfile']['name']) == ""){
+	
+	
+	$con = mysql_connect("localhost","davis","davis");
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	mysql_select_db("davisanddavis", $con);
+	
+	$sql="UPDATE groups SET group_name='$name', short_name='$_POST[short_name]' description='$description;' , active='$_POST[group2]' WHERE id='$_POST[proid]'";
+	
+	
+	if (!mysql_query($sql,$con))
+	{
+		die('Error: ' . mysql_error());
+	}
+	echo "1 record added";
+	
+	mysql_close($con);
+	header('Location: ../admin.php');
+	
+}
+else{
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+	echo "success";
+} else{
+	error_reporting(E_ALL);
+	echo "There was an error uploading the file, please try again!";
+	echo $_FILES['uploadedfile']['tmp_name'] . "target:" . $target_path;
+}
+
+error_reporting(E_ALL && ~E_NOTICE);
+
+$target_path = basename( $_FILES['uploadedfile']['name']); //get filename without directory
+
+$con = mysql_connect("localhost","davis","davis");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("davisanddavis", $con);
+
+$sql="UPDATE products SET product_name='$name', description='$description' , recipe='$_POST[prod_recip]' , prod_type='$_POST[group1]' , img_filename='$target_path' , cost='$_POST[prod_price]', active='$_POST[group2]' WHERE id='$_POST[proid]'";
+
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+
+mysql_close($con);
+header('Location: ../admin.php');
+}
+?>
