d7687a8bcc42ce240288c06cce9ed2cbc91e279a
Andy Singley <asingley@m17x.(none)>
2012-01-28 23:20:53 -0500

    group generator

--- group_generator.php
+++ group_generator.php
@@ -1,1 +1,21 @@
-hi
+<script type="text/javascript" src="scripter.js"></script>
+<link rel="stylesheet" type="text/css" href="styler.css" />
+<?php 
+$con = mysql_connect("localhost","davis","davis");
+if (!$con)
+{
+	die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("davisanddavis", $con);
+$sql= "SELECT * FROM products where id=$proid";
+$result = mysql_query($sql);
+$row = mysql_fetch_array($result);
+
+echo '$row[group_name]';
+?>
+		<div class="purpBar">
+			<img src="images/purplefadebar.jpg" alt="" />
+		</div>
+		
+<?php mysql_close($con);?>
