2cc99a3ed24490a1c07a3d8f1330aafb7e2a4a41
Andy Singley <asingley@m17x.(none)>
2012-02-04 18:59:37 -0500

    shopsite

--- public_html/hparty.php
+++ public_html/hparty.php
@@ -1,4 +1,4 @@
-<div><a href="partyRequest.htm">More Information</a>&nbsp;&nbsp;&bull;&nbsp;&nbsp;<a href="partyConsultant.htm">Consultant Area</a></div>
+<div><a href="../index.php?proid=partyrequest">More Information</a>&nbsp;&nbsp;&bull;&nbsp;&nbsp;<a href="../index.php?proid=partyConsultant">Consultant Area</a></div>
 		<div id="prodTitle">
 				<div>HOME PARTY</div>
 		</div>
