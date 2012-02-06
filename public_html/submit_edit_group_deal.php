a49aa13b71d0018ea784b87e8e65197ec34bdbe0
Andy Singley <asingley@m17x.(none)>
2012-02-05 21:48:56 -0500

    working on deals

---
+++ public_html/submit_edit_group_deal.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if(!session_is_registered(myusername)){
+	header("location:index.php");
+}
+
+$name = base64_encode($_POST[deal_name]);
+$con = mysql_connect("localhost","davis","davis");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("davisanddavis", $con);
+
+$sql="UPDATE group_deals SET shop_id='$POST[shop_id]', deal_name='$name', active='$_POST[group2]' WHERE id='$_POST[proid]'";
+
+
+
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+
+mysql_close($con);
+header('Location: ../admin.php');
+?>
