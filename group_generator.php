a32fc86cc6858e823c6aa7fe22363724fefecbcb
Andy Singley <asingley@m17x.(none)>
2012-02-05 22:55:44 -0500

    working on deals

--- group_generator.php
+++ group_generator.php
@@ -22,6 +22,21 @@ echo '<center><H2>' . base64_decode($row['group_name']) . '</H2></center>';
 $sqlOne = "SELECT * FROM products WHERE prod_type='$proid' AND active=1";
 $resultOne = mysql_query($sqlOne);
 
+echo '<select id="selOpt">';
+
+$sqlGroup= "SELECT * FROM group_deals WHERE active=1 AND prod_type=$proid";
+$resultGroup = mysql_query($sqlGroup);
+
+while ($rowGroup = mysql_fetch_array($resultGroup))
+{
+	echo '<option value="'.$rowGroup['shop_id'].'">'.base64_decode($rowGroup['deal_name']).'</option>';
+}
+
+
+
+echo '</optgroup>';
+echo '</select><br>';
+echo '<img onclick="fAddItem(\''.$proid.'\');" class="cart" src="images/cart.gif" alt="Add this selection to your basket." />';
 echo htmlspecialchars_decode(base64_decode($row['description']));
 echo '<div id="prodDisp">';
 while ($rowOne = mysql_fetch_array($resultOne))
