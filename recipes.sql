94625254689da21d31f221377f94caaba71ab50b
Andy Singley <asingley@m17x.(none)>
2012-02-10 20:39:58 -0500

    final changes

--- recipes.sql
+++ recipes.sql
@@ -2,6 +2,7 @@ CREATE TABLE `recipes` (
 `id` int(4) NOT NULL auto_increment,
 `recipe_name` varchar(65) NOT NULL default '',
 `description` text NOT NULL default '',
+`product_id` int(6),
 `active`	int(2),
 PRIMARY KEY (`id`)
 ) TYPE=MyISAM AUTO_INCREMENT=2 ;
